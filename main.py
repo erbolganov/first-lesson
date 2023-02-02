77271c7075dcf0e9e2f91dc2365a6eb70428a6c2
erbolganov <azamat0816erbolganov@gmail.com>
2023-02-02 23:48:25 +0500

    types of data

---
+++ main.py
@@ -0,0 +1,42 @@
+# 1-zadaniya
+print('1-task')
+name = "Jon"
+admin = name
+print(admin)
+
+#2-zadaniya
+print('2-task')
+a1 = 5 + 3
+a2 = 5 - 3
+a3 = 5 * 3
+a4 = 5 / 3
+print(f"5+3={a1}, 5-3={a2}, 5*3={a3}, 5/3={a4}")
+
+#3-zadaniya
+print('3-task')
+a6 = 5 % 3
+a7 = 3 % 5
+a8 = 5 + int('3')
+a9 = int('5') - 3
+a10 = str(75) + 'kg'
+print(a6, a7, a8, a9, a10 )
+
+# 4-zadaniya
+print('1-task')
+height = int(input("enter height: "))
+width = int(input("enter width: "))
+s = height * width
+print(f"square {s}")
+
+# 5- zadaniya
+
+name = input("Enter name")
+admin = name
+print(admin)
+
+print("-----------------------------------------------------")
+a1 = input()
+a2 = 5
+a3 = 5 * 3
+a4 = 5 / 3
+print(f"5+3={a1}, 5-3={a2}, 5*3={a3}, 5/3={a4}")
